c47e76efd07d875cac7d7a0f2e27bc29ba657b48
Qiu-Nick <nicolas.theaux.peria@gmail.com>
2024-12-19 19:55:30 +0100

    refactor: clean up HTML, manifest, and service worker formatting

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,21 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('my-cache').then((cache) => {
-        return cache.addAll([
-          '/', // L'URL principale
-          '/index.html', // Fichiers HTML nécessaires
-          '/static/css/main.css', // CSS nécessaires
-          '/static/js/main.js', // JS nécessaires
-          '/favicon.ico', // Icône
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/static/css/main.css',
+        '/static/js/main.js',
+        '/favicon.ico',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,28 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // Enregistre automatiquement le service worker
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'], // Assure-toi d'avoir ces fichiers dans ton public
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
-        name: 'Nom de ton App',
-        short_name: 'Nom court',
-        description: 'Description de ton application',
+        name: "Survivor's Map",
+        short_name: 'Map',
+        description: 'Une carte pour survivants',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '.',
+        start_url: '/',
         icons: [
           {
-            src: '/icon.png', // Remplace par le chemin vers ton icône
+            src: 'icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon.png',
+            src: 'icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
